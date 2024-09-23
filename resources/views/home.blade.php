c4e88e34ae2c779a13ad9f3293be04571382d19e
AJ Javadi <amirjavadi25@gmail.com>
2024-09-23 17:04:17 -0500

    Removed Tailwind CSS and cleaned up stylesheet. Imported Bulma and testing out home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CTV Marketing Agency</title>
+    {{--    BULMA CDN --}}
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+    >
+    {{--    TODO: add meta tags for SEO --}}
+</head>
+<body>
+{{--HERO SECTION--}}
+
+<section class="hero is-primary is-fullheight">
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <h1 class="title">
+                Welcome to our CTV Marketing Agency
+            </h1>
+            <h2 class="subtitle">
+                Innovative Solutions for Connected TV Advertising
+            </h2>
+            <a href="{{ route('contact.show') }}" class="button-is-link-is-large">Contact Us</a>
+        </div>
+    </div>
+</section>
+
+{{--SERVICES SECTION --}}
+
+</body>
+</html>
