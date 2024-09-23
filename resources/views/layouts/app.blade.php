4d34ebd7c768b0d971e39007dedb9306775ff299
AJ Javadi <amirjavadi25@gmail.com>
2024-09-23 16:46:59 -0500

    Removed Tailwind CSS and cleaned up stylesheet. Imported Bulma and testing out home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,39 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('Three First Names & Associates')</title>
+{{--    BULMA CDN --}}
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+    >
+</head>
+<body>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<! ----- >
+<nav class="bulma-navbar-brand">
+    <ul class="navbar-link">
+        <li class="bulma-navbar-link">
+            <a class="navbar-link">Home Page</a>
+            <a class="navbar-link">Home Page</a>
+            <a class="navbar-link">Home Page</a>
+            <a class="navbar-link">Home Page</a>
+        </li>
+    </ul>
+</nav>
+{{--Heading section--}}
+<section id="heading-section">
+    <h1 class="title"> Three First Names & Assocites</h1>
+    <p class="subtitle"> subtitle placeholder </p>
+</section>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+
+
+
+</body>
 </html>
